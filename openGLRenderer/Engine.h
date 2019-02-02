35c2ac9d7776cde7d32938ff00ed3aad83e6ba97
Mishal Zaman <mishalzaman@gmail.com>
2019-02-01 22:06:51 -0500

    move main process to engine

--- openGLRenderer/Engine.h
+++ openGLRenderer/Engine.h
@@ -2,7 +2,14 @@
 
 #include <GL/glew.h>
 #include <sdl/SDL.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <iostream>
+
+class CameraFP;
+class Input;
+class SceneManager;
 
 class Engine
 {
@@ -11,14 +18,22 @@ public:
 	~Engine();
 	bool initialize();
 	void load();
-	void update();
+	void update(float deltaTime);
 	void render();
+	void bufferUpdate();
 	void cleanUp();
-	SDL_Window *window;
-	SDL_GLContext context;
+	bool shutdown();
 private:
 	int screenWidth;
 	int screenHeight;
+	bool quit = false;
+	glm::mat4 projection;
+	glm::mat4 view;
+	SDL_Window *window;
+	SDL_GLContext context;
 	SDL_Event event;
+	CameraFP* camera;
+	Input* input;
+	SceneManager* sceneManager;
 };
 
--- openGLRenderer/main.cpp
+++ openGLRenderer/main.cpp
@@ -9,113 +9,34 @@
 #include "Player.h"
 #include "Input.h"
 #include "SceneManager.h"
+#include "Engine.h"
 
-bool firstMouse = true;
-const int SCREEN_WIDTH = 1280;
-const int SCREEN_HEIGHT = 720;
-bool quit = false;
+Engine* engine = new Engine(1280, 720);
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-	SDL_Event event;
+	engine->initialize();
 
-	// Initialize SDL's Video subsystem
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		std::cout << "Failed to init SDL\n";
-		return false;
-	}
+	engine->load();
 
-	// Create window
-	SDL_Window *window = SDL_CreateWindow(
-		"Test",
-		SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED,
-		SCREEN_WIDTH,
-		SCREEN_HEIGHT,
-		SDL_WINDOW_OPENGL
-	);
+	float deltaTime, lastTime = 0.0f;
 
-	if (!window)
-	{
-		std::cout << "Unable to create window\n";
-		return false;
-	}
-
-	// Create context
-	SDL_GLContext context = SDL_GL_CreateContext(window);
-	SDL_CaptureMouse(SDL_TRUE);
-	SDL_ShowCursor(0);
-	// SDL_SetWindowFullscreen(window, SDL_TRUE);
-
-	// initialize glew
-	glewExperimental = GL_TRUE;
-	glewInit();
-
-	// Set OpenGL attributes
-	int SDL_GL_SetAttribute(SDL_GLattr attr, int value);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-
-	glEnable(GL_DEPTH_TEST);
-
-	SDL_WarpMouseInWindow(window, SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT / 2.0f);
-
-
-	CameraFP *camera = new CameraFP(SCREEN_WIDTH, SCREEN_HEIGHT);
-	Input input = Input();
-
-	//setup scene
-	SceneManager* scene = new SceneManager();
-	scene->loadScene("scene.txt");
-
-	float deltaTime = 0.0f; 
-	float lastTime = 0.0f; 
-
-	while (!quit) {
+	while (!engine->shutdown()) {
 		float currentTime = SDL_GetTicks();
 		deltaTime = currentTime - lastTime;
 		lastTime = currentTime;
 
-		// reset buffer
-		glClearColor(0.0, 0.1, 0.0, 1.0);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-		glEnable(GL_CULL_FACE);
-		glCullFace(GL_BACK);
+		engine->bufferUpdate();
 
-		input.update(deltaTime);
-		if (input.isForward()) { camera->forward(deltaTime); };
-		if (input.isBackward()) { camera->backward(deltaTime); };
-		if (input.isStrafeLeft()) { camera->strafeLeft(deltaTime); };
-		if (input.isStrafeRight()) { camera->strafeRight(deltaTime); };
-		if (input.isMouseMotion())
-		{
-			int x, y;
-			SDL_GetMouseState(&x, &y);
-			camera->mousePositionUpdate(deltaTime, x, y);
-			SDL_WarpMouseInWindow(window, SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT / 2.0f);
-		}
-		if (input.isQuit()) { quit = true; }
-			
-		glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
-		glm::mat4 view = camera->getViewMatrix();
+		engine->update(deltaTime);
 
-		scene->render(view, projection);
-
-		SDL_GL_SwapWindow(window);
+		engine->render();
 	}
 
-	delete(camera);
-	delete(scene);
-
-	SDL_GL_DeleteContext(context);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
+	engine->cleanUp();
+	delete(engine);
 
 	return 0;
 }
